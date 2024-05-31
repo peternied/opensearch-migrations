1d5bfc5412d72925233cb0a846c7387e78d485f2
Peter Nied <peternied@hotmail.com>
2024-05-31 15:20:57 +0000

    Not really working...
    Signed-off-by: Peter Nied <peternied@hotmail.com>

--- RFS/src/test/java/com/rfs/framework/SimpleRestoreFromSnapshot_ES_7_10.java
+++ RFS/src/test/java/com/rfs/framework/SimpleRestoreFromSnapshot_ES_7_10.java
@@ -19,11 +19,20 @@ import com.rfs.version_es_7_10.IndexMetadataFactory_ES_7_10;
 import com.rfs.version_es_7_10.ShardMetadataFactory_ES_7_10;
 import com.rfs.version_es_7_10.SnapshotRepoProvider_ES_7_10;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
 import io.opentelemetry.sdk.metrics.data.MetricData;
+import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.testing.exporter.InMemoryMetricReader;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 
 /**
  * Simplified version of RFS for use in testing - ES 7.10 version.
@@ -32,20 +41,36 @@ public class SimpleRestoreFromSnapshot_ES_7_10 {
 
     private static final Logger logger = LogManager.getLogger(SimpleRestoreFromSnapshot_ES_7_10.class);
     private final InMemoryMetricReader metricReader = InMemoryMetricReader.create();
+    private final Resource resource = Resource.getDefault().toBuilder().put(AttributeKey.stringKey("service.name"), "rfs").put(AttributeKey.stringKey("service.version"), "1.0").build();
+
     private final SdkMeterProvider meterProvider = SdkMeterProvider.builder()
             .registerMetricReader(metricReader)
+            .setResource(resource)
             .build();
+    private final SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+        .addSpanProcessor(SimpleSpanProcessor.create(LoggingSpanExporter.create()))
+        .setResource(resource)
+        .build();
+    private final SdkLoggerProvider sdkLoggerProvider = SdkLoggerProvider.builder()
+        .addLogRecordProcessor(BatchLogRecordProcessor.builder(SystemOutLogRecordExporter.create()).build())
+        .setResource(resource)
+        .build();
 
     public SimpleRestoreFromSnapshot_ES_7_10() {
-        OpenTelemetrySdk.builder()
-                .setMeterProvider(meterProvider)
-                .buildAndRegisterGlobal();
+        // GlobalOpenTelemetry.get();
+        // GlobalOpenTelemetry.resetForTest();
+        // OpenTelemetrySdk.builder()
+        //         .setMeterProvider(meterProvider)
+        //         .setTracerProvider(sdkTracerProvider)
+        //         .setLoggerProvider(sdkLoggerProvider)
+        //         .buildAndRegisterGlobal();
     }
 
     public List<MetricData> getMetrics(final String metricName) {
+        GlobalOpenTelemetry.get().getMeterProvider().get("metricName").
         return metricReader.collectAllMetrics()
             .stream()
-            .map(m -> { logger.debug("Metric: " + m.getName()); return m;})
+            .map(m -> { logger.debug(m); return m;})
             .filter(metric -> metric.getName().equals(metricName))
             .collect(Collectors.toList());
     }
