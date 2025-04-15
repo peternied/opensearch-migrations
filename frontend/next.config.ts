00c594f7618967490747ae3fd6164a904f63c596
Peter Nied <peternied@hotmail.com>
2025-04-15 17:06:45 +0000

    Remove non-relevant files for initial site
    Signed-off-by: Peter Nied <peternied@hotmail.com>

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,27 +1,4 @@
 import type { NextConfig } from 'next';
-
-const { execSync } = require('child_process');
-
-function getGitMostRecentTag() {
-  return execOrUnknown('git describe --tags --abbrev=0 HEAD');
-}
-
-function getGitCommitHash() {
-  return execOrUnknown('git rev-parse --short HEAD');
-}
-
-function getGitCommitDate() {
-  return execOrUnknown('git show -s --format=%ci HEAD');
-}
-
-function execOrUnknown(command: string) {
-  try {
-    return execSync(command).toString().trim();
-  } catch (e) {
-    return 'unknown';
-  }
-}
-
 const nextConfig: NextConfig = {
   transpilePackages: [
     '@cloudscape-design/components',
@@ -29,9 +6,6 @@ const nextConfig: NextConfig = {
   ],
   output: 'export',
   env: {
-    COMMIT_RECENT_TAG: getGitMostRecentTag(),
-    COMMIT_SHA: getGitCommitHash(),
-    COMMIT_DATE: getGitCommitDate()
   },
   distDir: 'build/site'
 };
