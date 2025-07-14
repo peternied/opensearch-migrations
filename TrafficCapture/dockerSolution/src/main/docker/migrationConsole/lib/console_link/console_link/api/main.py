ddbacb91da85b961fe1be1bffd09d1819e0229df
Peter Nied <peternied@hotmail.com>
2025-07-14 21:23:50 +0000

    Start api server and root to /api on deployments
    Signed-off-by: Peter Nied <peternied@hotmail.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/console_link/api/main.py
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/console_link/api/main.py
@@ -1,10 +1,12 @@
+import os
 from fastapi import FastAPI
 from console_link.api.metadata import metadata_router
 from console_link.api.clusters import router as cluster_router
 
 app = FastAPI(
     title="Migration Assistant API",
-    version="0.0.1"
+    version="0.0.1",
+    root_path=os.getenv("FASTAPI_ROOT_PATH", "")
 )
 
 app.include_router(metadata_router)
--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/start-console.sh
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/start-console.sh
@@ -9,6 +9,7 @@ echo "Starting API server..."
 mkdir -p /var/log
 
 cd /root/lib/console_link
+export FASTAPI_ROOT_PATH=/api
 pipenv run gunicorn console_link.api.main:app \
     -k uvicorn.workers.UvicornWorker \
     -w 4 \
