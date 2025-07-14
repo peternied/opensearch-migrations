a6c5463c8ed6f89668ecc8f7ae1f2b8c102e5823
Peter Nied <peternied@hotmail.com>
2025-07-14 20:00:42 +0000

    Start the API server on the MA console
    Signed-off-by: Peter Nied <peternied@hotmail.com>

---
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/start-console.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+# if [[ -f /root/loadServicesFromParameterStore.sh ]]; then
+#   /root/loadServicesFromParameterStore.sh
+# fi
+
+echo "Starting API server..."
+mkdir -p /var/log
+
+cd /root/lib/console_link
+pipenv run gunicorn console_link.api.main:app \
+    -k uvicorn.workers.UvicornWorker \
+    -w 4 \
+    -b 0.0.0.0:8000 \
+    --access-logfile /var/log/gunicorn-access.log \
+    --error-logfile /var/log/gunicorn-error.log &
+
+sleep 2
+
+tail /var/log/gunicorn-access.log
+tail /var/log/gunicorn-error.log
+
+cd /root
+
+# Switch to $SHARED_LOGS_DIR_PATH/api/logs/access.log
+#           $SHARED_LOGS_DIR_PATH/api/logs/error.log
+
+# Keep container running and interactive
+exec tail -f /dev/null
